cabae8311fe8514dee014e89cc2217d449284207
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-09-03 22:30:34 -0500

    ki-nav

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,79 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/pages/Auth/Auth';
+import EditItem from '../components/pages/EditItem/EditItem';
+import Home from '../components/pages/Home/Home';
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import NewItem from '../components/pages/NewItem/NewItem';
+import SingleItem from '../components/pages/SingleItem/SingleItem';
+
+import fbConnection from '../helpers/data/connection';
+
 import './App.scss';
 
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <h2>React Hoarder</h2>
-        <button className="btn btn-info"><i className="fas fa-hard-hat"></i> I am a button <i className="fas fa-hard-hat"></i></button>
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar authed={authed} />
+            <div className="container">
+              <Switch>
+                <PrivateRoute path="/home" component={Home} authed={authed} />
+                <PrivateRoute path="/new" component={NewItem} authed={authed} />
+                <PrivateRoute path="/edit/:ItemId" component={EditItem} authed={authed} />
+                <PrivateRoute path="/Items/:ItemId" component={SingleItem} authed={authed} />
+                <PublicRoute path="/auth" component={Auth} authed={authed} />
+                <Redirect from="*" to="/home"/>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './Home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>INSIDE Home COMPONENT</h2>
+        <button className="btn btn-info">I am a button</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
